32cfc9639429d87a95e1bf2214aae912dd1f3eaf
Mike Pierce <info@michaelpierce.trade>
2019-09-23 23:26:01 +0200

    Add logic for creating Timing app tasks

--- index.js
+++ index.js
@@ -3,17 +3,66 @@ import yargs from 'yargs';
 import tl from './src/teamleader';
 import ta from './src/timing';
 
+const tempProjects = {
+  data: [
+    {
+      id: '624ca743-8998-4f8c-add1-c427bb022166',
+      reference: 'PRO-2',
+      title: 'New company website',
+      description: '',
+      status: 'active',
+      customer: {
+        type: 'contact',
+        id: 'abbf02c0-8ff9-4048-b83f-5195035161e1',
+      },
+      starts_on: '2016-02-04',
+      due_on: '2016-10-14',
+      source: {
+        type: 'deal',
+        id: '5023d7c2-80d7-4d4b-b2bd-0fcaa6a1f069',
+      },
+    },
+  ],
+};
+
+const tempMilestones = {
+  data: [
+    {
+      id: 'cfb4146d-06be-41f1-bb39-aa3c929c71dc',
+      project: {
+        type: 'project',
+        id: '944534fb-15f1-4eea-aab1-82a427aa2d0d',
+      },
+      due_on: '2018-01-01',
+      name: 'Initial setup',
+      responsible_user: {
+        type: 'user',
+        id: 'e1240972-6cfc-4549-b49c-edda7568cc48',
+      },
+      status: 'done',
+      invoicing_method: 'time_and_materials',
+    },
+  ],
+};
+
 const syncProjects = async () => {
-  const tlProjects = await tl.getProjects();
+  // const tlProjects = await tl.getProjects();
+  const tlProjects = tempProjects;
+  // console.log(tlProjects);
 
-  _.each(tlProjects, tlProject => {
-    const tlMilestones = tl.getMilestones(tlProject.id);
-    // ta.upsertProject(tlProject);
-    console.log(tlProject);
+  _.each(tlProjects.data, async tlProject => {
+    // const tlMilestones = await tl.getMilestones(tlProject.id);
+    const tlMilestones = tempMilestones;
+    const parent = await ta.upsertProject(tlProject);
+    console.log({ parent });
 
-    _.each(tlMilestones, tlMilestone => {
-      // ta.upsertProject(tlMilestone);
-      console.log(tlMilestone);
+    _.each(tlMilestones.data, async tlMilestone => {
+      const child = await ta.upsertProject({
+        parent: parent && parent.data && parent.data.self,
+        ...tlMilestone,
+      });
+
+      console.log({ child });
     });
   });
 };
@@ -22,38 +71,43 @@ const syncTimesheets = () => {
   console.log(ta);
 };
 
-const { initialize, authorize, execute } = yargs
+const { initializeTL, authorizeTL, projects, timesheets } = yargs
   .usage('Usage: -n <name>')
-  .option('init', {
-    alias: 'initialize',
+  .option('i', {
+    alias: 'initializeTL',
     describe: 'Generate authentication details for Teamleader access',
-    type: 'string',
+    type: 'boolean',
   })
-  .option('auth', {
-    alias: 'authorize',
+  .option('a', {
+    alias: 'authorizeTL',
     describe:
       'Redirect URI generated from init. Will authenticate the app and store details in .env',
     type: 'string',
     requiresArg: true,
   })
-  .option('e', {
-    alias: 'execute',
-    describe:
-      'The task you want to execute.\r\n: Options are "projects" or "timesheets"',
-    type: 'string',
-    choices: ['projects', 'timesheets'],
+  .option('p', {
+    alias: 'projects',
+    describe: 'Sync projects and milestones from Teamleader to Timing app',
+    type: 'boolean',
+  })
+  .option('t', {
+    alias: 'timesheets',
+    describe: 'Sync tasks from Timing App to Teamleader',
+    type: 'boolean',
   }).argv;
 
-if (initialize) {
+if (initializeTL) {
   tl.initialize();
 }
 
-if (authorize) {
-  tl.authorize(authorize);
+if (authorizeTL) {
+  tl.authorize(authorizeTL);
 }
 
-if (execute === 'projects') {
+if (projects) {
   syncProjects();
-} else if (execute === 'timesheets') {
+}
+
+if (timesheets) {
   syncTimesheets();
 }
--- src/timing.js
+++ src/timing.js
@@ -3,40 +3,78 @@ import Axios from 'axios';
 import _ from 'lodash';
 import self from './timing';
 
-const env = { TIMING_API_KEY: 'blah' };
+const { TIMING_API_KEY, TIMING_NAMESPACE } = process.env;
 
 export default {
-  request: async (endpoint, params) => {
+  request: async (endpoint, method, params = {}) => {
     try {
-      const success = await Axios.get(endpoint, {
-        params,
+      const success = await Axios({
+        url: endpoint,
+        method,
+        data: params,
         headers: {
-          Authorization: `Bearer ${env.TEAMLEADER_API_KEY}`,
+          Authorization: `Bearer ${TIMING_API_KEY}`,
         },
       });
 
-      console.log(success.data);
+      return success.data;
     } catch (error) {
       console.log(error);
     }
   },
 
-  endpoint: action => action && `https://web.timingapp.com/api/v1/${action}`,
+  endpoint: action => action && `https://web.timingapp.com/api/v1${action}`,
 
-  getProjects: () => {},
+  getRoot: async () => {
+    const root = await self.upsertProject({
+      title: TIMING_NAMESPACE,
+      parent: null,
+    });
 
-  getProject: projectTitle =>
-    _.head(_.filter(self.getProjects(), { title: projectTitle })),
+    console.log({ root });
 
-  updateProject: project => {
-    self.request(self.endpoint(project.id, 'PUT'), project);
+    return root.data.self;
   },
 
-  insertProject: project => {
-    self.request(self.endpoint(project.id, 'POST'), project);
+  getProjects: async () => self.request(self.endpoint('/projects'), 'GET'),
+
+  getProject: async projectTitle => {
+    const search = await self.request(self.endpoint('/projects'), 'GET', {
+      title: projectTitle,
+    });
+
+    return (search.data && search.data[0]) || null;
   },
 
-  upsertProject: project => {},
+  updateProject: async project =>
+    await self.request(self.endpoint(project.self), 'PUT', project),
+
+  insertProject: async project => {
+    const normalizeProject = {
+      title: project.title || project.name,
+      parent:
+        project.parent ||
+        (project.parent === undefined && (await self.getRoot())) ||
+        null,
+    };
+
+    return await self.request(
+      self.endpoint('/projects'),
+      'POST',
+      normalizeProject
+    );
+  },
+
+  upsertProject: async project => {
+    const exists = await self.getProject(project.title || project.name);
+    console.log({ exists });
+
+    if (exists) {
+      return self.updateProject(exists);
+    }
+
+    return self.insertProject(project);
+  },
 
   getTasks: (start, end) => {},
 };
